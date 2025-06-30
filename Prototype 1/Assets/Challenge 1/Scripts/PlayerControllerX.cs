c80b2a51614f3f80291412bdf87f393fadb5195b
Arkaenaz <flooper2234@gmail.com>
2025-07-01 01:20:14 +0800

    Challenge 1
    Completion of Challenge 1 in Unity's Junior Programmer's Pathway

---
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _rotationSpeed;
+    private float _verticalInput;
+
+    private InputAction _moveAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        _verticalInput = _moveAction.ReadValue<Vector2>().y;
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left * _rotationSpeed * _verticalInput * Time.deltaTime);
+    }
+}
